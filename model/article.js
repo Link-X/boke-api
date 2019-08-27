f004039b1b2a80e98eeef84f83bb517f9ff80bf0
许道斌 <xudb01@mingyuanyun.com>
2019-08-27 14:25:56 +0800

    -

--- model/article.js
+++ model/article.js
@@ -53,7 +53,7 @@ module.exports = {
         return new Promise((res, rej) => {
             const sql = `SELECT * from article WHERE id = ${params.id}`
             connection.query(sql, (err, data) => {
-                if (!err && data && data.length) {
+                if (!err && data && data.length && userData && userData.data && userData.data.id) {
                     redisMode.readArticle(params, userData)
                     redisMode.getArticleReadLength(params).then(readLength => {
                         const resData = { ...data[0], ...{ readNumber: readLength } }
